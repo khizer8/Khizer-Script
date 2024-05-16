4692cdd1602a6b2fb98c822279dc56e3ec57801b
khizer8 <168194309+khizer8@users.noreply.github.com>
2024-05-16 09:59:04 -0400

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,19 @@
+import nmap
+
+scanner = nmap.PortScanner()
+
+
+target = "127.0.0.1"
+
+
+scanner.scan(target)
+
+
+for host in scanner.all_hosts():
+    print("Host: ", host)
+    print("State: ", scanner[host].state())
+    for proto in scanner[host].all_protocols():
+        print("Protocol: ", proto)
+        ports = scanner[host][proto].keys()
+        for port in ports:
+            print("Port: ", port, "State: ", scanner[host][proto][port]['state'])
